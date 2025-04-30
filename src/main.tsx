d84f94b089e76edbaca9d048e01cbb783ae84be8
vsahal <vsahal@Vs-MacBook-Air.local>
2025-04-30 01:18:08 -0500

    update scheduleDate state to fix iphone edit bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
+import { Authenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from "aws-amplify";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from "@aws-amplify/ui-react";
+import outputs from "../amplify_outputs.json";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
